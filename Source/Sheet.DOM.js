71ac8d63f80172e5452bc770c67348aa22917a69
Thomas Aylott <oblivious+git@subtlegradient.com>
2010-05-16 03:59:03 -0400

    improved DOM styleSheet maker

--- Source/Sheet.DOM.js
+++ Source/Sheet.DOM.js
@@ -15,11 +15,43 @@ if (typeof Sheet == 'undefined') var Sheet = {};
 if (Sheet.DOM == null) Sheet.DOM = {};
 
 Sheet.DOM.createSheet = function(raw){
-	var div = document.createElement('div');
-	div.innerHTML = '<style type="text/css">' + raw + '</style>';
-	document.getElementsByTagName('html')[0].appendChild(div);
-	return document.styleSheets[document.styleSheets.length - 1];
+	var oldLength = document.styleSheets.length;
+	
+	if (document.createStyleSheet){
+		document.createStyleSheet();
+		document.styleSheets[document.styleSheets.length - 1].cssText = raw;
+	}
+	
+	if (oldLength >= document.styleSheets.length){
+		var style = document.createElement('style');
+		style.setAttribute('type','text/css');
+		style.appendChild(document.createTextNode(raw));
+		document.getElementsByTagName('head')[0].appendChild(style);
+	}
+	
+	if (oldLength >= document.styleSheets.length){
+		var style = document.createElement('div');
+		style.innerHTML = '<style type="text/css">' + raw + '</style>';
+		document.getElementsByTagName('head')[0].appendChild(style);
+	}
+	
+	if (oldLength >= document.styleSheets.length)
+		throw new Error('no styleSheet added :(');
+	
+	var sheet = document.styleSheets[document.styleSheets.length - 1];
+	sheet.cssText = raw;
+	
+	return sheet;
 };
+
+// Sheet.DOM.createSheet = function(raw){
+// 	var style = document.createElement('style');
+// 	style.setAttribute('type','text/css');
+// 	style.appendChild(document.createTextNode(raw));
+// 	document.getElementsByTagName('head')[0].appendChild(style);
+// 	
+// 	return document.styleSheets[document.styleSheets.length - 1];
+// };
 
 Sheet.DOM.createStyle = function(raw){
 	var div = document.createElement('div');
