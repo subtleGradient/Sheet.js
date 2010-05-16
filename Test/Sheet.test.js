fff0e1cf3db345e7fff25c3e30f1506d09da654d
Thomas Aylott <oblivious+git@subtlegradient.com>
2010-05-16 03:57:27 -0400

    Unify test runners

--- Test/Sheet.test.js
+++ Test/Sheet.test.js
@@ -1,31 +1,25 @@
 // -*- Mode: JavaScript QUnit; tab-width: 4; -*-
 
-if (typeof exports == 'undefined') var exports = this;
+if (typeof exports == 'undefined') var exports = {};
 
-// If run from the project root
+// Test Requirements
+
 if (typeof require != 'undefined') {
-	if (typeof print === 'undefined') var print = require('sys').print;
-	require.paths.push('Source');
-	require.paths.push('Test');
-	require.paths.push('Test/lib');
+	// If run from the project root
+	require.paths.unshift('Source');
+	require.paths.unshift('Test');
+	
+	if (typeof API == 'undefined')
+		var API = require("DOM-styleSheet.api");
+	
+	if (typeof Sheet == 'undefined')
+		var Sheet = require("Sheet").Sheet;
 }
 
-// requirements
-if (typeof API === 'undefined') var API = require("./DOM-styleSheet.api");
-if (typeof Sheet === 'undefined') var Sheet = require("Sheet").Sheet;
+// Test
 
+exports ["test Sheet.js | DOM styleSheet"] = 
+	API ["DOM styleSheet"] (Sheet);
 
-if (typeof QUnit != 'undefined') QUnit.module ("new Sheet with string")
-exports ["test DOM styleSheet"] = API ["DOM styleSheet"] (Sheet);
-exports ["test DOM style attribute"] = API ["DOM style attribute"] (Sheet);
-
-
-if (typeof run == 'undefined'){
-	if (require.main == module.id) // Narwhal
-		require("os").exit(require("test/runner").run(exports));
-	if ('.' == module.id) // Node.js
-		process.exit(require("sg-testrunner").run(exports));
-} else run(exports); // QUnit
-
-
-if (typeof QUnit != 'undefined') QUnit.module ("");
+exports ["test Sheet.js | DOM style attribute"] = 
+	API ["DOM style attribute"] (Sheet);
