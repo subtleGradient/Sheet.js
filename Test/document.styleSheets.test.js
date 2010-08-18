7a0e95d6ba44751a4832d3da0e522ef19a38d7d6
Thomas Aylott <oblivious+git@subtlegradient.com>
2010-08-18 06:02:29 -0400

    make it clear that the browser tests are testing the browser

--- Test/document.styleSheets.test.js
+++ Test/document.styleSheets.test.js
@@ -1,7 +1,7 @@
 if (typeof exports == 'undefined') var exports = {};
 
-exports ["test (document.styleSheets) DOM styleSheet"] = 
+exports ["test Browser Native (document.styleSheets) DOM styleSheet"] = 
 	API ["DOM styleSheet"] (Sheet.DOM.createSheet);
 
-exports ["test (document.styleSheets) DOM style attribute"] = 
+exports ["test Browser Native (document.styleSheets) DOM style attribute"] = 
 	API ["DOM style attribute"] (Sheet.DOM.createStyle);
