fef9f457cb7c1d139fde2480ee29a24918b73470
Thomas Aylott <oblivious+git@subtlegradient.com>
2010-11-28 00:57:04 -0800

    fixme: disallow strings in selectors until there are tests for it

--- Source/SheetParser.CSS.js
+++ Source/SheetParser.CSS.js
@@ -126,7 +126,7 @@ CSS.atRule = x([CSS.at, ';'])
 ;(CSS.comment = x(/\/\*\s*((?:[^*]|\*(?!\/))*)\s*\*\//))
 .names=[                   'comment']
 
-;(CSS.selector = x(/\s*((\d+%)|[^\{}]+?)\s*/))
+;(CSS.selector = x(/\s*((\d+%)|[^\{}'"]+)\s*/))
 .names=[               'selectorText','keyText']
 
 ;(CSS.block = x(/\{\s*((?:[^{}]|\{(?:[^{}]|\{(?:[^{}]|\{(?:[^{}]|\{[^{}]*\})*\})*\})*\})*)\s*\}/))
