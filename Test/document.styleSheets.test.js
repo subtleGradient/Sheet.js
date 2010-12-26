1efb389cc6434ffc7a8662ba4b9df374e7a00edd
Thomas Aylott <oblivious+git@subtlegradient.com>
2010-12-25 19:48:32 -0800

    Cleanup Sheet.DOM API to make a bit more sense.

--- Test/document.styleSheets.test.js
+++ Test/document.styleSheets.test.js
@@ -4,4 +4,4 @@ exports ["test Browser Native (document.styleSheets) DOM styleSheet"] =
 	API ["DOM styleSheet"] (Sheet.DOM.createSheet);
 
 exports ["test Browser Native (document.styleSheets) DOM style attribute"] = 
-	API ["DOM style attribute"] (Sheet.DOM.createStyle);
+	API ["DOM style attribute"] (Sheet.DOM.createStyleWrapped);
